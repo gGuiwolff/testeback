bb97d4a4614b7362ee5b7053f0bd0fea90f5ba4b
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-25 03:14:32 -0300

    Update welcome.js

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 exports.getWelcome = async (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "../..", "public", "index.html"));
 };
 
 /*exports.getWelcomes = async (req, res) => {
