65d21dca9c9c525edc8fd9b2a63c820e45d7349c
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-07-13 14:08:28 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,13 @@
 exports.requireLoggedOutUser = (req, res, next) => {
     if (req.session.userId) {
+        console.log('[VODE ESTA ATUTENTICADO]')
         return res.redirect("/");
     }
     next();
 };
 exports.requireLoggedInUser = (req, res, next) => {
     if (!req.session.userId) {
+        console.log('[NAO ATUTENTICADO]')
         return res.redirect("/welcome");
     }
     next();
